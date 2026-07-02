3ada979cd064bf83ad05b19a9ad49cbd1b01cccc
smilefinancesolution01-coder <smilefinancesolution01@gmail.com>
2026-07-02 13:22:32 +0530

    Update Firebase configuration with project details

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,22 +1,31 @@
 // Firebase Configuration
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyAaCzz5rnxjs4rnxQ80r2ttL7lb82wwIl8",
+  authDomain: "kfk-marketplace.firebaseapp.com",
+  projectId: "kfk-marketplace",
+  storageBucket: "kfk-marketplace.firebasestorage.app",
+  messagingSenderId: "516025410635",
+  appId: "1:516025410635:web:7d5fd031fd90e339d259ce",
+  measurementId: "G-C04V7V6ZQW"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
+
 const auth = getAuth(app);
+
 const db = getFirestore(app);
 
-export { app, analytics, auth, db };
+export {
+    app,
+    analytics,
+    auth,
+    db
+};
