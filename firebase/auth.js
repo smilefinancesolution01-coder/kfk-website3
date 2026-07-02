7c8f572b5217e81f59676e61341c1433d4c348ac
smilefinancesolution01-coder <smilefinancesolution01@gmail.com>
2026-07-02 13:33:01 +0530

    Add authentication functions for admin login/logout
    Implement admin login, logout, and check login functions using Firebase Auth.

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,1 +1,45 @@
-// Auth
+import { auth } from "./firebase-config.js";
+
+import {
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// ==========================
+// LOGIN
+// ==========================
+
+export async function adminLogin(email, password) {
+
+return await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+}
+
+// ==========================
+// LOGOUT
+// ==========================
+
+export async function adminLogout() {
+
+return await signOut(auth);
+
+}
+
+// ==========================
+// CHECK LOGIN
+// ==========================
+
+export function checkLogin(callback){
+
+onAuthStateChanged(auth,(user)=>{
+
+callback(user);
+
+});
+
+}
