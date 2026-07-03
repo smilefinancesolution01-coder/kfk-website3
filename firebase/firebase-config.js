5fbd24d95c21ff66db9edfdac3a33674b6ed6d2b
smilefinancesolution01-coder <smilefinancesolution01@gmail.com>
2026-07-03 18:03:07 +0530

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new credentials.

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,12 +1,16 @@
+// ======================================================
+// KFK Firebase Configuration
+// File : js/firebase-config.js
+// ======================================================
+
 const firebaseConfig = {
-
-    apiKey:"",
-    authDomain:"",
-    projectId:"",
-    storageBucket:"",
-    messagingSenderId:"",
-    appId:""
-
+  apiKey: "AIzaSyAaCzz5rnxjs4rnxQ80r2ttL7lb82wwIl8",
+  authDomain: "kfk-marketplace.firebaseapp.com",
+  projectId: "kfk-marketplace",
+  storageBucket: "kfk-marketplace.firebasestorage.app",
+  messagingSenderId: "516025410635",
+  appId: "1:516025410635:web:7d5fd031fd90e339d259ce",
+  measurementId: "G-C04V7V6ZQW"
 };
 
 export default firebaseConfig;
